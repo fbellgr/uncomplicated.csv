8cde829369045d530f87a0b81e0cff4a0fe307ed
fbellgr <>
2015-03-06 23:09:40 -0500

    Minor reader/writer settings improvements Read to DataTable

---
+++ Uncomplicated.Csv/Uncomplicated.Csv/CsvUtil.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Uncomplicated.Csv
+{
+	public class CsvUtil
+	{
+		public static DataTable GetDataTable(Stream stream, bool columnNamesInFirstRow, CsvReaderSettings settings)
+		{
+			DataTable dt = new DataTable();
+
+			using (CsvReader reader = new CsvReader(stream, settings))
+			{
+
+				// noms des colonnes
+				if (columnNamesInFirstRow)
+				{
+					CreateHeaders(reader, dt);
+				}
+
+				string[] row = null;
+				int ri = 0;
+				while ((row = reader.Read()) != null)
+				{
+					if (ri == 0 && !columnNamesInFirstRow)
+					{
+						for (int i = 0; i < row.Length; ++i)
+						{
+							dt.Columns[i].ColumnName = string.Concat("F", i + 1);
+						}
+					}
+					dt.Rows.Add(row.Select(r => r.Trim()).ToArray());
+					++ri;
+				}
+			}
+
+			return dt;
+		}
+
+		/// <summary>
+		/// Définition des noms de colonne à partir de la première ligne
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <param name="dt"></param>
+		private static void CreateHeaders(CsvReader reader, DataTable dt)
+		{
+			string[] headers = reader.Read();
+			if (headers != null)
+			{
+				int columnIndex = 1;
+				foreach (string header in headers)
+				{
+					string name = (header ?? string.Empty).Trim();
+					bool empty = name.Length == 0;
+					if (empty)
+					{
+						// cellule vide, pas de nom
+						// on en génère un mais cette colonne ne pourra pas être utilisée
+						Console.Error.WriteLine("Empty column name in table '{0}': {1}", dt.TableName, columnIndex + 1);
+						int i = 0;
+						while (dt.Columns.Contains(name))
+						{
+							name = string.Concat("F", columnIndex + i);
+							++i;
+						}
+					}
+					else
+					{
+						int i = 1;
+						if (dt.Columns.Contains(name))
+						{
+							// nom de colonne déjà utilisé
+							// on en génère un mais cette colonne ne pourra pas être utilisée
+
+							Console.Error.WriteLine("Redundant column name in table '{0}'({1}): {2}", dt.TableName, columnIndex + 1, name);
+							while (dt.Columns.Contains(name))
+							{
+								name = string.Concat(name, i);
+								++i;
+							}
+						}
+					}
+
+					dt.Columns.Add(name, typeof(string));
+
+					++columnIndex;
+				}
+			}
+		}
+	}
+}
